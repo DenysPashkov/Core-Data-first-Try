d2fa446afa69888e7fc5ac5041eba6b350eb2511
dextronder97 <dexixoxa@yahoo.it>
2020-06-08 19:39:28 +0200

    first commit

---
+++ Core Data Try/AddCharacterView.swift
@@ -0,0 +1,21 @@
+//
+//  AddCharacterView.swift
+//  Core Data Try
+//
+//  Created by denys on 19/01/2020.
+//  Copyright © 2020 denys. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddCharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddCharacterView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCharacterView()
+    }
+}
---
+++ Core Data Try/MyCharacter.swift
@@ -0,0 +1,9 @@
+//
+//  MyCharacter.swift
+//  Core Data Try
+//
+//  Created by denys on 19/01/2020.
+//  Copyright © 2020 denys. All rights reserved.
+//
+
+import Foundation
